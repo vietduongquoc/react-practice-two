2ba87494b99aaad6c27e382c06f94e17fe68e11e
VietDuong <duongquocviet@gmail.com>
2024-06-27 16:25:41 +0700

    Deploy Vercel

--- react-practice-two/src/App.jsx
+++ react-practice-two/src/App.jsx
@@ -5,7 +5,7 @@ import HomePage from './pages/HomePage/HomePage';
 import RegisterPage from './pages/RegisterPage/Register';
 import PreviewPage from './pages/PreviewPage/Preview';
 import Loading from './components/Spinner/Loading';
-import MyShelf from './pages/MyShelf/MyShelf'
+import MyShelf from './pages/myShelf/MyShelf'
 
 const App = () => {
     return (
